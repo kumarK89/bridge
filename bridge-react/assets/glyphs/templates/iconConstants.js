829a379d3691ad861688b4d5abf2ebb661584e7c
kranthiB <kranthi.b76@gmail.com>
2016-11-18 17:07:52 +0530

    Removed un-necessary

--- bridge-react/assets/glyphs/templates/iconConstants.js
+++
@@ -1,4 +0,0 @@
-// WARNING: This is a generated file that shouldn't be edited. To regenerate
-// this file, run the FontCustom compiler: `fontcustom compile`
-<% @glyphs.each do |name, value| %>
-exports.ICON_GS_<%= name.to_s.upcase.gsub(/-/,"_") %> = "\u<%= value[:codepoint].to_s(16) %>";<% end %>
